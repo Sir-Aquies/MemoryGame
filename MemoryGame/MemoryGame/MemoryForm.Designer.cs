9a144ab151d44956da0ac2048e40b60f9a723e8e
Jhair Castillo <93554657+Sir-Aquies@users.noreply.github.com>
2022-07-11 14:07:03 -0500

    Project Files
    The WinForms project itself

---
+++ MemoryGame/MemoryGame/MemoryForm.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace MemoryGame
+{
+    partial class MemoryForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MemoryForm));
+            this.GrayPic = new System.Windows.Forms.PictureBox();
+            this.selectButton = new System.Windows.Forms.Button();
+            this.tilePanel = new System.Windows.Forms.Panel();
+            this.sizeValue = new System.Windows.Forms.TextBox();
+            this.movesLabel = new System.Windows.Forms.Label();
+            this.movesValues = new System.Windows.Forms.Label();
+            this.darkmode = new System.Windows.Forms.CheckBox();
+            this.rainbowModeCheckBox = new System.Windows.Forms.CheckBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.GrayPic)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // GrayPic
+            // 
+            this.GrayPic.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.GrayPic.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.GrayPic.Enabled = false;
+            this.GrayPic.Image = ((System.Drawing.Image)(resources.GetObject("GrayPic.Image")));
+            this.GrayPic.Location = new System.Drawing.Point(12, 9);
+            this.GrayPic.Name = "GrayPic";
+            this.GrayPic.Size = new System.Drawing.Size(64, 67);
+            this.GrayPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.GrayPic.TabIndex = 9;
+            this.GrayPic.TabStop = false;
+            this.GrayPic.Visible = false;
+            // 
+            // selectButton
+            // 
+            this.selectButton.BackColor = System.Drawing.Color.Black;
+            this.selectButton.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.selectButton.FlatAppearance.BorderSize = 20;
+            this.selectButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.selectButton.ForeColor = System.Drawing.Color.White;
+            this.selectButton.Location = new System.Drawing.Point(64, 281);
+            this.selectButton.Name = "selectButton";
+            this.selectButton.Size = new System.Drawing.Size(142, 61);
+            this.selectButton.TabIndex = 11;
+            this.selectButton.Text = "Select Size";
+            this.selectButton.UseVisualStyleBackColor = false;
+            this.selectButton.Click += new System.EventHandler(this.SelectButton_Click);
+            // 
+            // tilePanel
+            // 
+            this.tilePanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tilePanel.Location = new System.Drawing.Point(292, 9);
+            this.tilePanel.MaximumSize = new System.Drawing.Size(780, 640);
+            this.tilePanel.Name = "tilePanel";
+            this.tilePanel.Padding = new System.Windows.Forms.Padding(10);
+            this.tilePanel.Size = new System.Drawing.Size(780, 640);
+            this.tilePanel.TabIndex = 12;
+            this.tilePanel.TabStop = true;
+            this.tilePanel.Visible = false;
+            // 
+            // sizeValue
+            // 
+            this.sizeValue.BackColor = System.Drawing.Color.Black;
+            this.sizeValue.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.sizeValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sizeValue.ForeColor = System.Drawing.Color.White;
+            this.sizeValue.Location = new System.Drawing.Point(64, 213);
+            this.sizeValue.Name = "sizeValue";
+            this.sizeValue.Size = new System.Drawing.Size(142, 62);
+            this.sizeValue.TabIndex = 13;
+            // 
+            // movesLabel
+            // 
+            this.movesLabel.AutoSize = true;
+            this.movesLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 33.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.movesLabel.ForeColor = System.Drawing.Color.White;
+            this.movesLabel.Location = new System.Drawing.Point(14, 11);
+            this.movesLabel.Name = "movesLabel";
+            this.movesLabel.Size = new System.Drawing.Size(180, 52);
+            this.movesLabel.TabIndex = 14;
+            this.movesLabel.Text = "Moves: ";
+            // 
+            // movesValues
+            // 
+            this.movesValues.AutoSize = true;
+            this.movesValues.Font = new System.Drawing.Font("Microsoft Sans Serif", 33.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.movesValues.ForeColor = System.Drawing.Color.White;
+            this.movesValues.Location = new System.Drawing.Point(173, 11);
+            this.movesValues.Name = "movesValues";
+            this.movesValues.Size = new System.Drawing.Size(47, 52);
+            this.movesValues.TabIndex = 15;
+            this.movesValues.Text = "0";
+            // 
+            // darkmode
+            // 
+            this.darkmode.AutoSize = true;
+            this.darkmode.Checked = true;
+            this.darkmode.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.darkmode.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.darkmode.ForeColor = System.Drawing.Color.White;
+            this.darkmode.Location = new System.Drawing.Point(14, 110);
+            this.darkmode.Name = "darkmode";
+            this.darkmode.Size = new System.Drawing.Size(183, 41);
+            this.darkmode.TabIndex = 16;
+            this.darkmode.Text = "DarkMode";
+            this.darkmode.UseVisualStyleBackColor = true;
+            this.darkmode.CheckedChanged += new System.EventHandler(this.CheckBox1_CheckedChanged);
+            // 
+            // rainbowModeCheckBox
+            // 
+            this.rainbowModeCheckBox.AutoSize = true;
+            this.rainbowModeCheckBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rainbowModeCheckBox.ForeColor = System.Drawing.Color.White;
+            this.rainbowModeCheckBox.Location = new System.Drawing.Point(13, 66);
+            this.rainbowModeCheckBox.Name = "rainbowModeCheckBox";
+            this.rainbowModeCheckBox.Size = new System.Drawing.Size(239, 41);
+            this.rainbowModeCheckBox.TabIndex = 17;
+            this.rainbowModeCheckBox.Text = "RainbowMode";
+            this.rainbowModeCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.panel1.Controls.Add(this.movesValues);
+            this.panel1.Controls.Add(this.darkmode);
+            this.panel1.Controls.Add(this.rainbowModeCheckBox);
+            this.panel1.Controls.Add(this.movesLabel);
+            this.panel1.Location = new System.Drawing.Point(12, 475);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(261, 160);
+            this.panel1.TabIndex = 18;
+            // 
+            // MemoryForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1084, 661);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.tilePanel);
+            this.Controls.Add(this.GrayPic);
+            this.Controls.Add(this.selectButton);
+            this.Controls.Add(this.sizeValue);
+            this.ForeColor = System.Drawing.Color.White;
+            this.MaximizeBox = false;
+            this.Name = "MemoryForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MemoryGame";
+            ((System.ComponentModel.ISupportInitialize)(this.GrayPic)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox GrayPic;
+        private System.Windows.Forms.Button selectButton;
+        private System.Windows.Forms.Panel tilePanel;
+        private System.Windows.Forms.TextBox sizeValue;
+        private System.Windows.Forms.Label movesLabel;
+        private System.Windows.Forms.Label movesValues;
+        private System.Windows.Forms.CheckBox darkmode;
+        private System.Windows.Forms.CheckBox rainbowModeCheckBox;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ MemoryGame/MemoryGame/MemoryForm.cs
@@ -0,0 +1,431 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+
+namespace MemoryGame
+{
+    public partial class MemoryForm : Form
+    {
+        private List<Button> Pairs = new List<Button>();
+        private List<Button> tiles = new List<Button>();
+        private List<Button> lag = new List<Button>();
+
+        private static System.Timers.Timer aTimer;
+
+        private static System.Timers.Timer RainbowTimer;
+        private int R = 0;
+        private int G = 0;
+        private int B = 0;
+
+        private static Random rng = new Random();
+
+        public MemoryForm()
+        {
+            InitializeComponent();
+
+            
+        }
+        // panel 780, 640
+        private void TileLoad(int size, int Cols, int Rows)
+        {
+            //int[] arr = ColsAndRows(size);
+            //int Cols = arr[0];
+            //int Rows = arr[1];
+            int width = (tilePanel.Width - 2 * tilePanel.Padding.All) / Cols;
+            int height = (tilePanel.Height - 2 * tilePanel.Padding.All) / Rows;
+            int x = tilePanel.Padding.All;
+            int y = tilePanel.Padding.All;
+            int dx = width;
+            int dy = height;           
+
+            for (int i = 0; i < size; i++)
+            {
+                Button tile = new Button()
+                {
+                    Parent = tilePanel,
+                    //SizeMode = PictureBoxSizeMode.StretchImage,
+                    Size = new Size(width, height),
+                    Location = new Point(x, y),                   
+                    BackgroundImageLayout = ImageLayout.Stretch,
+                    //BorderStyle = BorderStyle.Fixed3D
+                    FlatStyle = FlatStyle.Standard,
+                    //BackColor = Color.Black,
+                    ForeColor = Color.Black,
+                    Font = new Font(familyName: "Times New Roman", width / 4)                    
+                };
+                if (!rainbowModeCheckBox.Checked)
+                {
+                    BackgroundImage = GrayPic.Image;
+                }
+                CheckForIllegalCrossThreadCalls = false;
+                tile.Click += Tile_Click;
+
+                tiles.Add(tile);
+
+                TilePopulate(tiles, size);
+
+                x += dx;
+                if (x >= ((Cols - 1) * dx) + width)
+                {
+                    y += dy;
+                    x = tilePanel.Padding.All;
+                }
+                
+            }
+        }
+
+        private void Tile_Click(object sender, EventArgs e)
+        {
+            if (lag.Count != 0)
+            {
+                return;
+            }
+            Button tile = (Button)sender;
+            tile.Enabled = false;
+            if (tile.Name.Contains("_"))
+            {
+                int last = tile.Name.Length - 2;
+                tile.Text = tile.Name.Remove(last);               
+            }
+            else
+            {
+                tile.Text = tile.Name;
+            }
+            
+
+            Pairs.Add(tile);
+
+            if (Pairs.Count == 2)
+            {
+                Moves();
+                if (Pairs[0].Name == $"{ Pairs[1].Name }_2" || $"{ Pairs[0].Name }_2" == $"{Pairs[1].Name}")
+                {
+                    Pairs[0].Visible = false;
+                    Pairs[1].Visible = false;
+
+                    Pairs[0].Enabled = false;
+                    Pairs[1].Enabled = false;
+
+                    tiles.Remove(Pairs[0]);
+                    tiles.Remove(Pairs[1]);
+
+                    Pairs = new List<Button>();
+                }
+                else
+                {
+                    lag.Add(Pairs[1]);
+                    aTimer = new System.Timers.Timer(580)
+                    {
+                        Enabled = true,
+                        AutoReset = false
+                    };
+                    Pairs[0].Enabled = true;
+                    Pairs[1].Enabled = true;
+                    Pairs[0].Text = "";                   
+                    aTimer.Elapsed += (source, ElapsedEventArgs) => { OnTimedEvent(source, ElapsedEventArgs); };
+                    Pairs = new List<Button>();                    
+                }
+
+                if (tiles.Count == 0)
+                {
+                    MessageBox.Show("Congratulations, You have Won.");
+                    Close();
+                }
+            }
+
+        }
+
+        private void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            lag[0].Text = "";
+            lag = new List<Button>();
+        }
+        private void TilePopulate(List<Button> tiles, int size)
+        {
+            var list = tiles.OrderBy(a => rng.Next()).ToList();
+            List<int> ints = new List<int>();
+
+            for (int i = 1; i <= size / 2; i++)
+            {
+                ints.Add(i);
+            }
+
+            for(int i = 0; i < (list.Count / 2); i++)
+            {
+                list[i].Name = $"{ ints[i] }";
+                list[i + (list.Count / 2)].Name = $"{ints[i]}_2";
+            }
+        }
+
+        private void SelectButton_Click(object sender, EventArgs e)
+        {
+            bool Even = int.TryParse(sizeValue.Text, out int size);
+
+            if (!Even || size == 2)
+            {
+                return;
+            }
+            if (size % 2 != 0)
+            {
+                return;
+            }
+
+            int[] CR = ColsAndRows(size);
+            if (CR[0] > (CR[1] + 50))
+            {
+                return;
+            }
+
+            sizeValue.Visible = false;
+            sizeValue.Enabled = false;
+            selectButton.Visible = false;
+            selectButton.Enabled = false;
+            tilePanel.Visible = true;
+
+            if (rainbowModeCheckBox.Checked)
+            {
+                RainbowMode();
+            }
+            
+            TileLoad(size, CR[0], CR[1]);
+        }
+
+        private List<int> Factorials(int factor)
+        {
+            List<int> output = new List<int>();
+            for (int i = 1; i < factor; i++)
+            {
+                if (factor % i == 0)
+                {
+                    output.Add(i);
+                }
+            }
+            return output;
+        }
+
+        private int[] ColsAndRows(int size)
+        {
+            List<int> factors = Factorials(size);
+            int[] output = new int[2];
+
+            factors.Reverse();
+
+            foreach (int i in factors)
+            {
+                foreach (int i2 in factors)
+                {
+                    if (i2 == i && i2 * i == size)
+                    {
+                        output[0] = i;
+                        output[1] = i;
+                        return output;
+                    }
+                    if ((i == i2 + 1 || i == i2 + 2) && i * i2 == size)
+                    {
+                        output[0] = i;
+                        output[1] = i2;
+                        return output;
+                    }
+                }
+            }
+
+            List<List<int>> ave = new List<List<int>>();
+
+            if (output[0] == 0 || output[1] == 0)
+            {
+                foreach (int i in factors)
+                {
+                    foreach (int j in factors)
+                    {
+                        if (i * j == size)
+                        {
+                            List<int> fac = new List<int>
+                            {
+                                i,
+                                j
+                            };
+                            ave.Add(fac);
+                        }
+                    }
+                }
+                double average = (ave[0][0] + ave[0][1]) / 2;
+                output[0] = ave[0][0];
+                output[1] = ave[0][1];
+
+                foreach (List<int> af in ave)
+                {
+                    double x = (double)(af[0] + af[1]) / 2;
+                    if (x < average)
+                    {
+                        average = x;
+                        output[0] = af[0];
+                        output[1] = af[1];
+                    }
+
+                }
+            }
+            return output;
+        }
+
+        private void Moves()
+        {
+            int moves = int.Parse(movesValues.Text);
+
+            moves++;
+
+            movesValues.Text = $"{ moves }";
+        }
+
+        private void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!darkmode.Checked)
+            {
+                BackColor = Color.White;
+                ForeColor = Color.Black;
+
+                selectButton.BackColor = Color.White;
+                selectButton.ForeColor = Color.Black;
+
+                sizeValue.BackColor = Color.White;
+                sizeValue.ForeColor = Color.Black;
+
+                darkmode.BackColor = Color.White;
+                darkmode.ForeColor = Color.Black;
+
+                movesLabel.BackColor = Color.White;
+                movesLabel.ForeColor = Color.Black;
+
+                movesValues.BackColor = Color.White;
+                movesValues.ForeColor = Color.Black;
+            }
+
+            if (darkmode.Checked)
+            {
+                BackColor = Color.Black;
+                ForeColor = Color.White;
+
+                selectButton.BackColor = Color.Black;
+                selectButton.ForeColor = Color.White;
+
+                sizeValue.BackColor = Color.Black;
+                sizeValue.ForeColor = Color.White;
+
+                darkmode.BackColor = Color.Black;
+                darkmode.ForeColor = Color.White;
+
+                movesLabel.BackColor = Color.Black;
+                movesLabel.ForeColor = Color.White;
+
+                movesValues.BackColor = Color.Black;
+                movesValues.ForeColor = Color.White;
+            }          
+        }
+
+        private void RainbowMode()
+        {
+            R = 255;
+            G = 0;
+            B = 0;
+            RainbowTimer = new System.Timers.Timer(500)
+            {
+                Enabled = true,
+                AutoReset = true
+            };
+
+            RainbowTimer.Elapsed += RainbowTimer_Elapsed;
+        }
+
+        private void RainbowTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            RainbowTimer.Stop();
+            if (R == 238 && G == 130 && B == 238)
+            {
+                R = 255;
+                G = 0;
+                B = 0;
+                SetColor();
+                RainbowTimer.Start();
+                return;
+            }
+            if (R == 255 && G == 0 && B == 0)
+            {
+                R = 255;
+                G = 165;
+                B = 0;
+                SetColor();
+                RainbowTimer.Start();
+                return;
+            }
+
+            if (R == 255 && G == 165 && B == 0)
+            {
+                R = 255;
+                G = 255;
+                B = 0;
+                SetColor();
+                RainbowTimer.Start();
+                return;
+            }
+
+            if (R == 255 && G == 255 && B == 0)
+            {
+                R = 0;
+                G = 255;
+                B = 0;
+                SetColor();
+                RainbowTimer.Start();
+                return;
+            }
+
+            if (R == 0 && G == 255 && B == 0)
+            {
+                R = 0;
+                G = 0;
+                B = 255;
+                SetColor();
+                RainbowTimer.Start();
+                return;
+            }
+
+            if (R == 0 && G == 0 && B == 255)
+            {
+                R = 75;
+                G = 0;
+                B = 130;
+                SetColor();
+                RainbowTimer.Start();
+                return;
+            }
+
+            if (R == 75 && G == 0 && B == 130)
+            {
+                R = 238;
+                G = 130;
+                B = 238;
+                SetColor();
+                RainbowTimer.Start();
+                return;
+            }
+        }
+        private void SetColor()
+        {
+            movesLabel.ForeColor = Color.FromArgb(R, G, B);
+            movesValues.ForeColor = Color.FromArgb(R, G, B);
+            darkmode.ForeColor = Color.FromArgb(R, G, B);
+            BackColor = Color.FromArgb(R, G, B);
+            rainbowModeCheckBox.ForeColor = Color.FromArgb(R, G, B);
+
+            foreach(Button b in tiles)
+            {
+                b.BackColor = Color.FromArgb(R, G, B);
+            }
+        }
+    }
+}
